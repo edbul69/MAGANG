2c544f79b8f47be896b0edbb9421dafa8cc12b2c
edbul69 <ejehezkiel@gmail.com>
2021-11-15 12:44:21 +0800

    Tampilan Home

--- application/views/home/Dashboard_view.php
+++ application/views/home/Dashboard_view.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
 
-    <title>SB Admin 2 - Dashboard</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet" />
-  </head>
-
-  <body id="page-top">
+<body id="page-top">
     <!-- Page Wrapper -->
     <div id="wrapper">
       <!-- Content Wrapper -->
@@ -71,7 +53,7 @@
               <li class="nav-item dropdown no-arrow">
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
-                  <img class="img-profile rounded-circle" src="img/undraw_profile.svg" />
+                  <img class="img-profile rounded-circle" src="assets/img/undraw_profile.svg" />
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -102,7 +84,7 @@
                   </a>
                   <div class="collapse hidden" id="collapseCardExample1">
                     <div class="card-body">
-                      <img src="img/890166_720.jpg" class="rounded-circle" alt="" width="100%" />
+                      <img src="assets\img\cctv.jpg" class="rounded-circle" alt="" width="100%" />
                       <div class="table-responsive">
                         <table class="table table-bordered" width="100%" cellspacing="0" style="margin-top: 2ch">
                           <tr>
@@ -132,7 +114,7 @@
                   </a>
                   <div class="collapse hidden" id="collapseCardExample2">
                     <div class="card-body">
-                      <img src="img/890166_720.jpg" class="rounded-circle" alt="" width="100%" />
+                      <img src="assets\img\cctv.jpg" class="rounded-circle" alt="" width="100%" />
                       <div class="table-responsive">
                         <table class="table table-bordered" width="100%" cellspacing="0" style="margin-top: 2ch">
                           <tr>
@@ -185,22 +167,4 @@
         </div>
       </div>
     </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
   </body>
-</html>
--- application/views/home/Home_view.php
+++ application/views/home/Home_view.php
@@ -1,63 +1,120 @@
-<!DOCTYPE html>
-<html lang="en">
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+      <!-- Content Wrapper -->
+      <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Main Content -->
+        <div id="content">
+          <!-- Topbar -->
+          <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+              <i class="fa fa-bars"></i>
+            </button>
+            
+            <a href="#" class="btn btn-primary btn-icon-split">
+                <span class="icon text-white-50">
+                    <i class="fas fa-arrow-right rotate-90"></i>
+                </span>
+                <span class="text">Dashboard</span>
+            </a>
 
-<head>
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ml-auto">
+              <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+              <li class="nav-item dropdown no-arrow d-sm-none">
+                <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-search fa-fw"></i>
+                </a>
+                <!-- Dropdown - Messages -->
+                <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                  <form class="form-inline mr-auto w-100 navbar-search">
+                    <div class="input-group">
+                      <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
+                      <div class="input-group-append">
+                        <button class="btn btn-primary" type="button">
+                          <i class="fas fa-search fa-sm"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </li>
 
-    <title>MAIN MENU</title>
-</head>
+              <div class="topbar-divider d-none d-sm-block"></div>
 
-<body>
-    <H3>DAFTAR IP & LOKASI</H3>
+              <!-- Nav Item - User Information -->
+              <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                  <img class="img-profile rounded-circle" src="assets/img/undraw_profile.svg" />
+                </a>
+                <!-- Dropdown - User Information -->
+                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Logout
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </nav>
+          <!-- End of Topbar -->
 
-    <table>
-        <form action="<?= base_url('home/insertip') ?>" method="post">
-            <tr>
-                <td>IP Address</td>
-                <td>:</td>
-                <td><input type="text" name="ip" required></td>
-            </tr>
-            <tr>
-                <td>Lokasi</td>
-                <td>:</td>
-                <td><input type="text" name="lokasi" required></td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="submit" value="add">
-                </td>
-            </tr>
-        </form>
-    </table>
-    <table>
-        <tr>
-            <td><b>IP Address</b></td>
-            <td><b>Lokasi</b></td>
-            <td><b>Status</b></td>
-        </tr>
-        <?php
-        $count = 0;
-        foreach ($data_cctv as $row) {
+           <!-- Begin Page Content -->
+           <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 class="h3 mb-0 text-gray-800">DAFTAR IP & LOKASI</h1>
+            </div>
+                <form class="table" action="<?= base_url('home/insertip') ?>" method="post">
+                        <tr>
+                            <td>IP Address</td>
+                            <td>:</td>
+                            <td><input  type="text" name="ip" required></td>
+                        </tr>
+                        <tr>
+                            <td>Lokasi</td>
+                            <td>:</td>
+                            <td><input type="text" name="lokasi" required></td>
+                        </tr>
+                        <input class="btn btn-primary" type="submit">
+                </form>
 
-            $count = $count + 1;
-        ?>
-            <tr>
-                <td><?php echo $row->ip ?></td>
-                <td><?php echo $row->lokasi ?></td>
-                <td>
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead?>    
+                <tr>
+                        <td><b>IP Address</b></td>
+                        <td><b>Lokasi</b></td>
+                        <td><b>Status</b></td>
+                    </tr>
+</thead>
                     <?php
-                    // $ip = $row->ip;
-                    // $ping = exec("ping -n 1 $ip", $output, $status);
-                    // if ($status === 0) {
-                    //     echo "Online";
-                    // } else {
-                    //     echo "Offline";
-                    // }
+                    $count = 0;
+                    foreach ($data_cctv as $row) {
 
+                        $count = $count + 1;
                     ?>
-                </td>
-            </tr>
-        <?php } ?>
-    </table>
+                    <tfoot>
+                        <tr>
+                            <td><?php echo $row->ip ?></td>
+                            <td><?php echo $row->lokasi ?></td>
+                            <td>
+                                <?php
+                                // $ip = $row->ip;
+                                // $ping = exec("ping -n 1 $ip", $output, $status);
+                                // if ($status === 0) {
+                                //     echo "Online";
+                                // } else {
+                                //     echo "Offline";
+                                // }
+
+                                ?>
+                            </td>
+                        </tr>
+                                </tfoot>
+                    <?php } ?>
+                </table>
+                    </div>
 </body>
 
-</html>
